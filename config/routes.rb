1ea6dc3759772e23dd9bf0e574d5980a7216b07a
Ubuntu <ubuntu@ip-172-31-10-201.ec2.internal>
2020-11-25 16:29:11 +0000

    Transition to book

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
+  resources :books
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   as :user do
     get "signin" => "devise/sessions#new"
     get "signup" => "devise/registrations#new"
+    delete "logout" => "destroy_user_session_path"
   end
+  get "books" => "books"
 end
