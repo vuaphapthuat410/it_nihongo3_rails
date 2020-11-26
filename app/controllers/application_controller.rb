9e6b8d3b5846f6a2b45eb692a194340dc416f410
Ubuntu <ubuntu@ip-172-31-10-201.ec2.internal>
2020-11-26 12:29:33 +0000

    add avatar on Users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
-class ApplicationController < ActionController::Base
+class ApplicationController < ActionController::Base # copy frpm sensei Anh 's code'
+	before_action :configure_permitted_parameters, if: :devise_controller?
+
+      protected
+         def configure_permitted_parameters
+             devise_parameter_sanitizer.permit :sign_up, keys: [:name, :age, :avatar]
+             devise_parameter_sanitizer.permit :account_update, keys: [:name, :age, :avatar]
+         end
 end
