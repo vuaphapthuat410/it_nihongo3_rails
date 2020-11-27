0b4bb26b1037a0578f50e897807efd87081ece71
Ubuntu <ubuntu@ip-172-31-10-201.ec2.internal>
2020-11-27 02:06:24 +0000

    Add detail book

---
+++ db/migrate/20201127015831_add_detail_to_books.rb
@@ -0,0 +1,5 @@
+class AddDetailToBooks < ActiveRecord::Migration[6.0]
+  def change
+    add_column :books, :detail, :text
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_26_043945) do
+ActiveRecord::Schema.define(version: 2020_11_27_015831) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2020_11_26_043945) do
     t.integer "number_of_pages"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "detail"
   end
 
   create_table "users", force: :cascade do |t|
